4aa8b820b8a0296f2c4c1f68c32b38e5608cb0cc
TeiHek <lerandy1235@gmail.com>
2023-03-09 11:57:35 -0800

    Initial commit

---
+++ Assets/Randy/Scripts/GenericObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class GenericObject : MonoBehaviour
+{
+    [SerializeField] Light2D _light;
+    [SerializeField] float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light2D>();
+        _light.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LightTimer()
+    {
+        yield return new WaitForSeconds(duration);
+        _light.enabled = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        _light.enabled = true;
+        StartCoroutine(LightTimer());
+    }
+}
