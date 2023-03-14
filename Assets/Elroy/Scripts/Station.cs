689978aefb0d8ec4ecf6f8b347679fc9bec66186
Elroy Saltzherr <lsaltzherr@gmail.com>
2023-03-14 12:37:32 -0700

    Make Elroy Scene

---
+++ Assets/Elroy/Scripts/Station.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station : MonoBehaviour
+{
+    [SerializeField] int radius;
+    [SerializeField] int ClaimRadius;
+    [SerializeField] int PulseSpeed;
+    [SerializeField] int CapturePings;
+    [SerializeField] int CurrentPings;
+    [SerializeField] int DeChargeRate;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(){
+
+        //}
+
+    }
+    void Pulse(){
+
+    }
+}
