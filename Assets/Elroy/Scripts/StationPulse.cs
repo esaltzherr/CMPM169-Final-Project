35f5d978c649e8d9b1be536c20331ede03ba8843
Elroy Saltzherr <lsaltzherr@gmail.com>
2023-03-16 14:02:45 -0700

    Stations Things

---
+++ Assets/Elroy/Scripts/StationPulse.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationPulse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Station station;
+    UnityEngine.Rendering.Universal.Light2D lights;
+    void Start()
+    {
+        station = GetComponentInParent<Station>();
+        lights = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D obj){
+        if (obj.gameObject.name == "Player"){
+            print(lights);
+        }
+        
+    }
+    
+    private void OnTriggerExit2D(Collider2D obj){
+        if (obj.gameObject.name == "Player"){
+            station.within = false;
+        }
+    }
+}
